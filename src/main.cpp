d96de0d759d5c02eb6fbac6db0ef2c9577da2d2d
Shri Hari <thisismeshrihari@gmail.com>
2026-04-12 17:50:34 +0530

    Fix Terminal left raw on Ctrl+C via RAII SIGINT handler

--- src/main.cpp
+++ src/main.cpp
@@ -1,193 +1,211 @@
-#include <iostream>
-#include <string>
-#include <vector>
+#include "Manager.h"
 #include <cstdio>
+#include <iostream>
+#include <memory>
+#include <string>
 #include <termios.h>
 #include <unistd.h>
-#include "Manager.h"
-#include "encryption.h"
+#include <csignal>
+#include <cstdlib>
 
 using std::cout;
 using std::endl;
 using std::string;
-using std::cin;
+
+// Global pointer for signal handler to restore terminal
+struct termios* g_OriginalTerminalState = nullptr;
+
+void sigintHandler(int signum) {
+  if (g_OriginalTerminalState) {
+    tcsetattr(STDIN_FILENO, TCSANOW, g_OriginalTerminalState);
+  }
+  exit(signum);
+}
+
+class TerminalStateRAII {
+  termios oldt;
+public:
+  TerminalStateRAII() {
+    tcgetattr(STDIN_FILENO, &oldt);
+    g_OriginalTerminalState = &oldt;
+  }
+  ~TerminalStateRAII() {
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    g_OriginalTerminalState = nullptr;
+  }
+  const termios& get() const { return oldt; }
+};
 
 // Function to read password with hidden input (asterisks shown in real-time)
 string readPasswordHidden() {
-    string password;
-    char ch;
+  string password;
+  char ch;
 
-    // Flush output buffer to ensure prompt is displayed
-    cout.flush();
-    fflush(stdout);
+  // Flush output buffer to ensure prompt is displayed
+  cout.flush();
+  fflush(stdout);
 
-    // Get current terminal settings
-    termios oldt, newt;
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
+  // Get current terminal settings via RAII
+  TerminalStateRAII termState;
+  termios newt = termState.get();
 
-    // Disable echo and set non-canonical mode
-    newt.c_lflag &= ~(ECHO | ICANON);
-    newt.c_cc[VMIN] = 1;
-    newt.c_cc[VTIME] = 0;
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+  // Disable echo and set non-canonical mode
+  newt.c_lflag &= ~(ECHO | ICANON);
+  newt.c_cc[VMIN] = 1;
+  newt.c_cc[VTIME] = 0;
+  tcsetattr(STDIN_FILENO, TCSANOW, &newt);
 
-    // Read password character by character
-    while (read(STDIN_FILENO, &ch, 1) == 1) {
-        if (ch == '\n') {
-            break;
-        }
-        if (ch == '\b' || ch == 127) {  // Backspace or DEL
-            if (!password.empty()) {
-                password.pop_back();
-                cout << "\b \b";
-                cout.flush();
-            }
-        } else {
-            password += ch;
-            cout << "*";
-            cout.flush();
-        }
+  // Read password character by character
+  while (read(STDIN_FILENO, &ch, 1) == 1) {
+    if (ch == '\n') {
+      break;
     }
+    if (ch == '\b' || ch == 127) { // Backspace or DEL
+      if (!password.empty()) {
+        password.pop_back();
+        cout << "\b \b";
+        cout.flush();
+      }
+    } else {
+      password += ch;
+      cout << "*";
+      cout.flush();
+    }
+  }
 
-    cout << endl;
+  cout << endl;
 
-    // Restore terminal settings
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-
-    return password;
+  return password;
 }
 
 void printHelp() {
-    cout << "Available commands:" << endl;
-    cout << "  create [accountName] - Create a new account (password will be prompted)" << endl;
-    cout << "  delete [accountName] - Delete an existing account (password will be prompted)" << endl;
-    cout << "  add [accountName] [username] - Add a password for a user (passwords will be prompted)" << endl;
-    cout << "  remove [accountName] [username] - Remove a password for a user (account password will be prompted)" << endl;
-    cout << "  view [accountName] [username] - View password for a user (account password will be prompted)" << endl;
+  cout << "Available commands:" << endl;
+  cout << "  create [accountName] - Create a new account (password will be "
+          "prompted)"
+       << endl;
+  cout << "  delete [accountName] - Delete an existing account (password will "
+          "be prompted)"
+       << endl;
+  cout << "  add [accountName] [username] - Add a password for a user "
+          "(passwords will be prompted)"
+       << endl;
+  cout << "  remove [accountName] [username] - Remove a password for a user "
+          "(account password will be prompted)"
+       << endl;
+  cout << "  view [accountName] [username] - View password for a user (account "
+          "password will be prompted)"
+       << endl;
 }
 
-int main(int argc, char* argv[]) {
-    // Basic command-line parsing
-    if (argc < 2) {
-        printHelp();
-        return 1;
+int main(int argc, char *argv[]) {
+  signal(SIGINT, sigintHandler);
+
+  // Basic command-line parsing
+  if (argc < 2) {
+    printHelp();
+    return 1;
+  }
+
+  // Load encryption type from config and create appropriate encryption object
+  std::string encType = getEncryptionTypeFromConfig();
+  auto encryption = createEncryptionObject(encType);
+  {
+    PasswordManager mgr(std::move(encryption));
+
+    string command = argv[1];
+    string accountName = argc > 2 ? argv[2] : "";
+    string accountPassword = "";
+    if (command != "create" && command != "delete" && command != "add" &&
+        command != "remove" && command != "view") {
+      cout << "Invalid command: " << command << endl;
+      printHelp();
+      return 1;
     }
+    if (command == "help") {
+      printHelp();
+    } else if (command == "create") {
+      if (argc != 3) {
+        cout << "Usage: passwordManager create [accountName]" << endl;
+        return 1;
+      }
 
-    // Load encryption type from config and create appropriate encryption object
-    string encType = getEncryptionTypeFromConfig();
-    Encryption* encryption = createEncryptionObject(encType);
-    {
-        PasswordManager mgr(encryption);
+      // Prompt for password
+      cout << "Enter account password: ";
+      accountPassword = readPasswordHidden();
 
-        string command = argv[1];
-        string accountName = argc > 2 ? argv[2] : "";
-        string accountPassword = argc > 3 ? argv[3] : "";
+      // Use AES encryption (only supported encryption type)
+      string encType = "aes";
 
-        // check if the command is valid
-        if(command != "create" && command != "delete" && command != "add" && command != "remove" && command != "view" && command != "config"){
-            cout << "Invalid command: " << command << endl;
-            printHelp();
-            delete encryption;
-            return 1;
-        }
-        if(command == "help"){
-            printHelp();
-        }
-        else if(command == "create"){
-            if (argc != 3) {
-                cout << "Usage: passwordManager create [accountName]" << endl;
-                delete encryption;
-                return 1;
-            }
+      if (mgr.createAccount(accountName, accountPassword, encType)) {
+        cout << "Account created successfully!" << endl;
+      }
+    } else if (command == "delete") {
+      if (argc != 3) {
+        cout << "Usage: passwordManager delete [accountName]" << endl;
+        return 1;
+      }
 
-            // Prompt for password
-            cout << "Enter account password: ";
-            accountPassword = readPasswordHidden();
+      // Prompt for password
+      cout << "Enter account password: ";
+      accountPassword = readPasswordHidden();
 
-            // Use AES encryption (only supported encryption type)
-            string encType = "aes";
+      bool x = mgr.deleteAccount(accountName, accountPassword);
+      if (x) {
+        cout << "Account deleted successfully!" << endl;
+      } else {
+        cout << "Account deletion failed or Account not found!" << endl;
+      }
+    } else if (command == "add") {
+      if (argc != 4) {
+        cout << "Usage: passwordManager add [accountName] [username]" << endl;
+        return 1;
+      }
 
-            if (mgr.createAccount(accountName, accountPassword, encType)) {
-                cout << "Account created successfully!" << endl;
-            }
-        }
-        else if(command == "delete"){
-            if (argc != 3) {
-                cout << "Usage: passwordManager delete [accountName]" << endl;
-                delete encryption;
-                return 1;
-            }
+      string username = argv[3];
 
-            // Prompt for password
-            cout << "Enter account password: ";
-            accountPassword = readPasswordHidden();
+      // Prompt for account password
+      cout << "Enter account password: ";
+      accountPassword = readPasswordHidden();
 
-            bool x = mgr.deleteAccount(accountName, accountPassword);
-            if(x){
-                cout << "Account deleted successfully!" << endl;
-            }
-            else{
-                cout << "Account deletion failed or Account not found!" << endl;
-            }
-        }
-        else if(command == "add"){
-            if (argc != 4) {
-                cout << "Usage: passwordManager add [accountName] [username]" << endl;
-                delete encryption;
-                return 1;
-            }
+      // Prompt for user password
+      cout << "Enter password for user '" << username << "': ";
+      string password = readPasswordHidden();
 
-            string username = argv[3];
+      mgr.addPassword(accountName, accountPassword, username, password);
+    } else if (command == "remove") {
+      if (argc != 4) {
+        cout << "Usage: passwordManager remove [accountName] [username]"
+             << endl;
+        return 1;
+      }
+      string username = argv[3];
 
-            // Prompt for account password
-            cout << "Enter account password: ";
-            accountPassword = readPasswordHidden();
+      // Prompt for account password
+      cout << "Enter account password: ";
+      accountPassword = readPasswordHidden();
 
-            // Prompt for user password
-            cout << "Enter password for user '" << username << "': ";
-            string password = readPasswordHidden();
+      bool x = mgr.deletePassword(accountName, accountPassword, username);
+      if (x) {
+        cout << "Password deleted successfully!" << endl;
+      } else {
+        cout << "Password deletion failed or Password not found!" << endl;
+      }
+    } else if (command == "view") {
+      if (argc != 4) {
+        cout << "Usage: passwordManager view [accountName] [username]" << endl;
+        return 1;
+      }
+      string username = argv[3];
 
-            mgr.addPassword(accountName, accountPassword, username, password);
-        }
-        else if(command == "remove"){
-            if (argc != 4) {
-                cout << "Usage: passwordManager remove [accountName] [username]" << endl;
-                delete encryption;
-                return 1;
-            }
-            string username = argv[3];
+      // Prompt for account password
+      cout << "Enter account password: ";
+      accountPassword = readPasswordHidden();
 
-            // Prompt for account password
-            cout << "Enter account password: ";
-            accountPassword = readPasswordHidden();
-
-            bool x = mgr.deletePassword(accountName, accountPassword, username);
-            if(x){
-                cout << "Password deleted successfully!" << endl;
-            }
-            else{
-                cout << "Password deletion failed or Password not found!" << endl;
-            }
-        }
-        else if(command == "view"){
-            if (argc != 4) {
-                cout << "Usage: passwordManager view [accountName] [username]" << endl;
-                delete encryption;
-                return 1;
-            }
-            string username = argv[3];
-
-            // Prompt for account password
-            cout << "Enter account password: ";
-            accountPassword = readPasswordHidden();
-
-            bool x = mgr.viewPasswords(accountName, accountPassword, username);
-            if(!x){
-                cout << "Failed to view passwords!" << endl;
-            }
-        }
-    }  // mgr goes out of scope and is destroyed here
-    delete encryption;
-    return 0;
+      bool x = mgr.viewPasswords(accountName, accountPassword, username);
+      if (!x) {
+        cout << "Failed to view passwords!" << endl;
+      }
+    }
+  }
+  return 0;
 }
